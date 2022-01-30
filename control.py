b4fc65f8383d691d5c0588eb8480cd95b178dd30
ShivaniV-98 <97776234+ShivaniV-98@users.noreply.github.com>
2022-01-29 18:39:55 -0800

    Update control.py

--- control.py
+++ control.py
@@ -98,8 +98,120 @@ class Controllers:
             logging.info("About to hit an obstacle @ {0}".format(cm))
             self.m.forward(0)
             
+class CVSteering:
+    
+    def __init__(self):
+        pass
+    
+    def start_cv(self): 
+        camera = cv2.VideoCapture(-1)
+        camera.set(3, 640)
+        camera.set(4, 480)
+        # while( camera.isOpened()):
+        _, image = camera.read()
+        return image        
+            # cv2.imshow('Original', image)
             
+        #b_w_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            # cv2.imshow('B/W', b_w_image)
             
+        #if cv2.waitKey(1) & 0xFF == ord('q') :
+        #    break
+            
+    cv2.destroyAllWindows()
+    
+    def look_for_color(self, frame):
+        upper_black = np.array([40,40,40])
+        lower_black = np.array([0,0,0])
+        # frame = cv2.imread('/home/pi/DeepPiCar/driver/data/road1_240x320.png')
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv,lower_black,upper_black)
+        edges = cv2.Canny(mask, 200, 400)
+        return edges
+    
+    def crop_video(self, edges):
+        # removing top of image
+        height, width = edges.shape
+        mask = np.zeros_like(edges)
+        polygon = np.array([[
+            (0, height * 1 / 2),
+            (width, height * 1 / 2),
+            (width, height),
+            (0, height),
+            ]], np.int32)
+        cv2.fillPoly(mask, polygon, 255)
+        cropped_edges = cv2.bitwise_and(edges, mask)
+        return cropped_edges
+        
+    def detect_line_segments(self, cropped_edges):    
+    
+        # looking for line segments
+        rho = 1  # distance precision in pixel, i.e. 1 pixel
+        angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
+        min_threshold = 10  # minimal of votes
+        line_segments = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold, 
+            np.array([]), minLineLength=8, maxLineGap=4)
+        return line_segments
+    
+    def average_slope_intercept(self, cvs, frame, line_segments): 
+        middle_lines = []
+        lane_lines = []
+        if line_segments is None:
+            # logging.info('No line_segment segments detected')
+            return []
+        
+        for line_segment in line_segments:
+            logging.info('line segment: {0}'.format(line_segment))
+            for x1, y1, x2, y2 in line_segment:
+                if x1 != x2:
+                    fit = np.polyfit((x1, x2), (y1, y2), 1)
+                    slope = fit[0]
+                    intercept = fit[1]
+                    middle_lines.append((slope, intercept)) 
+                else:
+                    logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
+                    
+        if middle_lines is None:
+            logging.info('All lines were vertical')
+            return []
+        lane_lines.append(cvs.make_points(frame, np.average(middle_lines,axis = 0)))
+        return lane_lines
+
+
+    def make_points(self, frame, line):
+        if line == []:
+            return []
+        else:
+            # logging.info("This is the line chosen by the car {0}".format(line))
+            height, width, _ = frame.shape
+            slope, intercept = line
+            y1 = height  # bottom of the frame
+            y2 = int(y1 * 1 / 2)  # make points from middle of the frame down
+        
+            # bound the coordinates within the frame
+            x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
+            x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
+            return [x1, y1, x2, y2]
+    
+    def steering_angle(self, path):
+        # logging.info('This is the paths found by the camera: {0}'.format(path))
+        if path !=[]:
+            x1, y1, x2, y2 = path[0]
+            x_offset = x2 - x1
+            y_offset = y2 - y1
+            drive_angle = math.atan(x_offset / y_offset * pi/ 180)
+        else:
+            drive_angle = 0
+        return drive_angle
+        
+    def steering_angle_adjustment(self, new_angle, turn_limit):
+        angle_diff = new_angle - Motors.steering_dir_val
+        if abs(angle_diff) > turn_limit:
+            adjusted_angle = Motors.steering_dir_val + turn_limit * angle_diff / abs(angle_diff)
+        else:
+            adjusted_angle = new_angle
+        logging.info('Angle from camera: {0}'.format(adjusted_angle))
+        return adjusted_angle  
             
 
 if __name__ == "__main__":
